7c1b9dcaa3f36fa0b5ba856e00bdaaaa8960ec66
Kanishk Katara <kanishkkatara@Kanishks-MacBook-Air.local>
2023-03-05 16:54:29 +0530

    Added UT

--- src/tests/mock/tracking.plan.mock.ts
+++ src/tests/mock/tracking.plan.mock.ts
@@ -22,7 +22,81 @@ const trackingPlanResult = {
   is_deleted: false,
 };
 
+const allTrackingPlansAndEventsRaw = [
+  {
+    tracking_plan_name: "TP 1",
+    tracking_plan_description: "Sample tracking plan",
+    event_name: "Event 1",
+    event_description: "",
+    event_rules: "{}",
+  },
+  {
+    tracking_plan_name: "TP 2",
+    tracking_plan_description: "Sample",
+    event_name: "event 1",
+    event_description: "",
+    event_rules: "{}",
+  },
+  {
+    tracking_plan_name: "TP 3",
+    tracking_plan_description: "test",
+    event_name: "E3",
+    event_description: "test",
+    event_rules: "{}",
+  },
+  {
+    tracking_plan_name: "TP 3",
+    tracking_plan_description: "test",
+    event_name: "E4",
+    event_description: "test",
+    event_rules: "{}",
+  },
+];
+
+const allTrackingPlansAndEvents = [
+  {
+    tracking_plan_name: "TP 1",
+    description: "Sample tracking plan",
+    event: [
+      {
+        name: "Event 1",
+        description: "",
+        rules: "{}",
+      },
+    ],
+  },
+  {
+    tracking_plan_name: "TP 2",
+    description: "Sample",
+    event: [
+      {
+        name: "event 1",
+        description: "",
+        rules: "{}",
+      },
+    ],
+  },
+  {
+    tracking_plan_name: "TP 3",
+    description: "test",
+    event: [
+      {
+        name: "E3",
+        description: "test",
+        rules: "{}",
+      },
+      {
+        name: "E4",
+        description: "test",
+        rules: "{}",
+      },
+    ],
+  },
+];
+
 export default {
-    trackingPlanModel,
-    trackingPlanResult
-}
+  trackingPlanModel,
+  trackingPlanResult,
+  allTrackingPlansAndEventsRaw,
+  allTrackingPlansAndEvents,
+};
--- src/tests/tracking.plan.test.ts
+++ src/tests/tracking.plan.test.ts
@@ -1,21 +1,40 @@
-
-import { assert } from 'chai';
-import sinon from 'sinon';
-import trackingPlanRepository from '../app/repository/tracking.plan.repository';
-import  trackingPlanService  from "../app/service/tracking.plan.service";
+import { assert } from "chai";
+import sinon from "sinon";
+import trackingPlanRepository from "../app/repository/tracking.plan.repository";
+import trackingPlanService from "../app/service/tracking.plan.service";
 import trackingPlanMock from "./mock/tracking.plan.mock";
 
-describe('createTrackingPlan', () => {
+describe("createTrackingPlan", () => {
   afterEach(() => {
     sinon.restore();
   });
 
-  it('Should create a new tracking plan and associated events in the database', async () => {
+  it("Should create a new tracking plan and associated events in the database", async () => {
     const trackingPlanModel = trackingPlanMock.trackingPlanModel;
-    const repositoryStub = sinon.stub(trackingPlanRepository, 'createTrackingPlan').resolves(trackingPlanMock.trackingPlanResult);
+    const repositoryStub = sinon
+      .stub(trackingPlanRepository, "createTrackingPlan")
+      .resolves(trackingPlanMock.trackingPlanResult);
     const expected = trackingPlanMock.trackingPlanResult;
-    const actual = await trackingPlanService.createTrackingPlan(trackingPlanModel);
+    const actual = await trackingPlanService.createTrackingPlan(
+      trackingPlanModel
+    );
     assert.equal(actual, expected);
-  })
-})
+    expect(repositoryStub.calledOnce).toEqual(true);
+  });
+});
 
+describe("getAllTrackingPlans", () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it("Should return an array of tracking plan objects with nested event objects", async () => {
+    const repositoryStub = sinon
+      .stub(trackingPlanRepository, "getAllTrackingPlans")
+      .resolves(trackingPlanMock.allTrackingPlansAndEventsRaw);
+    const expected = trackingPlanMock.allTrackingPlansAndEvents;
+    const actual = await trackingPlanService.getAllTrackingPlans();
+    assert.equal(JSON.stringify(actual), JSON.stringify(expected));
+    expect(repositoryStub.calledOnce).toEqual(true);
+  });
+});
